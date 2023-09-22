162625fa863e7c22e2494bcbb8ca5cf61adee89c
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2023-09-21 21:53:28 -0500

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,7 +1,10 @@
 class Solution(object):
     def reverse(self, x):
+        # 0 edge case
         if x == 0:
             return x
+        
+        #check for negative value and set flag
         neg = False
         if x < 0:
             neg = True
@@ -10,18 +13,23 @@ class Solution(object):
         value = 0
         counter = 0
         arr = []
+        # iterate through the input and append to arr
         while x > 0:
             arr.append(x%10)
             x= int(x/10)
+        # reverse the array and iterate 
         for idx in arr[::-1]:
-            # if counter == 1:
+            # rebuild the string with * 10 values
             value = value + idx * pow(10, counter)
             counter += 1
+        #check for negative
         if neg: 
             value = (value *-1)
+            #check for out of bounds range
             if value < pow(-2,31):
                 return 0
 
+        #check for out of bounds range
         if value > pow(2,31)-1:
             return 0
 
